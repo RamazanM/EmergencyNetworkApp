7e86bdbc3eb783caa3e8e3dcc1a4d1acc91cd48c
ozdemirdev <ttarikozdemirdev@hotmail.com>
2020-01-03 04:29:06 +0300

    First Aid page has done. Dot signs need to change.

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<sensors/SensorsPlugin.h>)
+#import <sensors/SensorsPlugin.h>
+#else
+@import sensors;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTSensorsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSensorsPlugin"]];
+}
+
+@end
--- lib/firstaid.dart
+++ lib/firstaid.dart
@@ -1,10 +1,88 @@
 import 'package:flutter/material.dart';
 import 'appbar.dart';
+import 'dart:convert';
 
-class FirstAidPage extends StatelessWidget{
+class FirstAidPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(appBar: appBar("İlkyardım"),);
+  State<StatefulWidget> createState() {
+    return FirstAidPageState();
+  }
+}
+
+class FirstAidPageState extends State {
+  var json = [];
+
+  void getJson() {
+    DefaultAssetBundle.of(context)
+        .loadString("assets/firstAid.json")
+        .then((data) {
+      setState(() {
+        json = jsonDecode(data);
+      });
+    });
   }
 
+  @override
+  void initState() {
+    getJson();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: appBar("İlkyardım"),
+        body: ListView.builder(
+          itemCount: json.length,
+          itemBuilder: (BuildContext context, int id) {
+            return ExpansionTile(
+              title: Text(json[id]["title"]),
+              children: _buildExpandableContent(json[id]),
+            );
+            //return containerBuilder(json[id]["title"]);
+          },
+        ));
+  }
+
+  List<Widget> _buildExpandableContent(dynamic json) {
+    List<Widget> columnContent = [];
+
+    for (var content in json["subtitles"])
+      columnContent.add(
+        new ListTile(
+          title: new Text(
+            content["title"],
+            style: new TextStyle(fontSize: 18.0),
+          ),
+          onTap: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    content: Form(
+                        child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Padding(
+                            padding: EdgeInsets.all(8),
+                            child: Text(
+                              content["title"],
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.red),
+                            )),
+                        Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Text(content["content"]),
+                        )
+                      ],
+                    )),
+                  );
+                });
+          },
+        ),
+      );
+
+    return columnContent;
+  }
 }
