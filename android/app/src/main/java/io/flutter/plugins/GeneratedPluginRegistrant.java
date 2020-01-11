147eb8cde430ba421ed2b9109224abc3eed7a619
RamazanM <r.ramazanmutlu@gmail.com>
2020-01-11 21:35:45 +0300

    for merge

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -1,7 +1,10 @@
 package io.flutter.plugins;
 
 import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugins.connectivity.ConnectivityPlugin;
 import io.flutter.plugins.sensors.SensorsPlugin;
+import com.example.wifi_configuration.WifiConfigurationPlugin;
+import com.alternadom.wifiiot.WifiIotPlugin;
 
 /**
  * Generated file. Do not edit.
@@ -11,7 +14,10 @@ public final class GeneratedPluginRegistrant {
     if (alreadyRegisteredWith(registry)) {
       return;
     }
+    ConnectivityPlugin.registerWith(registry.registrarFor("io.flutter.plugins.connectivity.ConnectivityPlugin"));
     SensorsPlugin.registerWith(registry.registrarFor("io.flutter.plugins.sensors.SensorsPlugin"));
+    WifiConfigurationPlugin.registerWith(registry.registrarFor("com.example.wifi_configuration.WifiConfigurationPlugin"));
+    WifiIotPlugin.registerWith(registry.registrarFor("com.alternadom.wifiiot.WifiIotPlugin"));
   }
 
   private static boolean alreadyRegisteredWith(PluginRegistry registry) {
